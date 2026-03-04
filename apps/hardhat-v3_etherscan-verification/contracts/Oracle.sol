1cf003ecceca51d6a7c21f498f20d9ef1935c8be
VGLoic <vincent.genod.loic@gmail.com>
2026-03-04 18:04:57 +0100

    format

--- apps/hardhat-v3_etherscan-verification/contracts/Oracle.sol
+++ apps/hardhat-v3_etherscan-verification/contracts/Oracle.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Oracle is Ownable {
     uint public by;
