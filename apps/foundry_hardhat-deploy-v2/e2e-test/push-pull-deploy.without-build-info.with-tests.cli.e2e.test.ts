9694c0db231fb03ef4f5e06270d15e9fe3b596c9
VGLoic <vincent.genod.loic@gmail.com>
2026-03-05 16:21:34 +0100

    extend test suites with CLI

---
+++ apps/foundry_hardhat-deploy-v2/e2e-test/push-pull-deploy.without-build-info.with-tests.cli.e2e.test.ts
@@ -0,0 +1,14 @@
+import { E2E_FOLDER_PATH } from "./e2e-folder-path.js";
+import { foundryDescribe } from "./foundry-describe.js";
+
+const outputArtifactsPath = `${E2E_FOLDER_PATH}/out-2026-cli-forge-default-full`;
+
+foundryDescribe({
+  title:
+    "[Foundry Hardhat-deploy v2] - Default compilation WITHOUT --build-info WITH test and scripts - Push artifact, pull artifact, deploy",
+  build: {
+    command: `forge build --out ${outputArtifactsPath} --cache-path ${outputArtifactsPath}-cache`,
+    outputArtifactsPath,
+  },
+  runner: "cli",
+});
