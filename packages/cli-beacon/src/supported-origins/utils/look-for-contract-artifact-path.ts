2827f2c823299da9fae04d5a21e8bac5f8a600dd
VGLoic <vincent.genod.loic@gmail.com>
2026-03-17 19:10:24 +0100

    fix path construction

--- packages/cli-beacon/src/supported-origins/utils/look-for-contract-artifact-path.ts
+++ packages/cli-beacon/src/supported-origins/utils/look-for-contract-artifact-path.ts
@@ -1,14 +1,16 @@
 import fs from "fs/promises";
+import path from "path";
 
 export async function* lookForContractArtifactPath(
-  path: string,
+  basePath: string,
 ): AsyncIterable<string> {
-  const entries = await fs.readdir(path, { withFileTypes: true });
+  const entries = await fs.readdir(basePath, { withFileTypes: true });
   for (const entry of entries) {
     if (entry.isDirectory() && entry.name !== "build-info") {
-      yield* lookForContractArtifactPath(`${path}/${entry.name}`);
+      const entryPath = path.join(basePath, entry.name);
+      yield* lookForContractArtifactPath(entryPath);
     } else if (entry.isFile() && entry.name.endsWith(".json")) {
-      yield `${path}/${entry.name}`;
+      yield path.join(basePath, entry.name);
     }
   }
 }
